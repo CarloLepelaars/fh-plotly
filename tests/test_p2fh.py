fd8d2edf77aede57cf5c032320899341e3e3775f
Carlo Lepelaars <info@carlolepelaars.nl>
2024-08-19 01:11:11 +0200

    Relax and update fasthtml

--- tests/test_p2fh.py
+++ tests/test_p2fh.py
@@ -1,4 +1,5 @@
 
+import fastcore
 import pandas as pd
 import plotly.express as px
 
@@ -8,7 +9,7 @@ def test_line():
     df = pd.DataFrame({'y': [1, 2, 3, 2], 'x': [3, 1, 2, 4]})
     fig = px.line(df, x='x', y='y')
     html_div = plotly2fasthtml(fig)
-    assert isinstance(html_div, list)
+    assert isinstance(html_div, fastcore.xml.FT)
 
 
 def test_bar():
@@ -16,14 +17,14 @@ def test_bar():
     fig = px.bar(df, x='x', y='y')
     html_div = plotly2fasthtml(fig)
     assert html_div is not None
-    assert isinstance(html_div, list)
+    assert isinstance(html_div, fastcore.xml.FT)
 
 def test_scatter():
     df = pd.DataFrame({'y': [1, 2, 3, 2], 'x': [3, 1, 2, 4], 'size': [10, 20, 30, 40]})
     fig = px.scatter(df, x='x', y='y', size='size')
     html_div = plotly2fasthtml(fig)
     assert html_div is not None
-    assert isinstance(html_div, list)
+    assert isinstance(html_div, fastcore.xml.FT)
 
 def test_3d_scatter():
     df = pd.DataFrame({
@@ -34,7 +35,7 @@ def test_3d_scatter():
     fig = px.scatter_3d(df, x='x', y='y', z='z')
     html_div = plotly2fasthtml(fig)
     assert html_div is not None
-    assert isinstance(html_div, list)
+    assert isinstance(html_div, fastcore.xml.FT)
 
 def test_3d_surface():
     df = pd.DataFrame({
@@ -46,4 +47,4 @@ def test_3d_surface():
     fig.update_traces(marker=dict(size=5, opacity=0.8))
     html_div = plotly2fasthtml(fig)
     assert html_div is not None
-    assert isinstance(html_div, list)
+    assert isinstance(html_div, fastcore.xml.FT)
